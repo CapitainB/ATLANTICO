57d3215f36ea852b3c001da1d3cb002bb3d36778
AngeloBot <33585124+AngeloBot@users.noreply.github.com>
2017-11-12 22:53:55 -0200

    adicionando arquivos arduino p/ biruta
    precisa-se checar se o codigo está certo devido ao fato de não ter funcionado. Led acendia msm sem o sensor e apenas com a aproximação da mão aos jumpers( q eram pra ser conectados ao sensor) sem o sensor de efeito hall. O teste individual é para apenas um sensor e o teste é para 3 sensores ao mesmo tempo com uma disposição espacial diferente: RIGHT, LEFT ou MID

---
+++ sensor_efeito_hall_teste.ino
@@ -0,0 +1,70 @@
+int statusLEFT = 0;
+int statusRIGHT = 0;
+int statusMID = 0;
+
+//pin para input proveniente sensores
+int pinLEFT = 3;
+int pinRIGHT = 5; 
+int pinMID = 6;
+
+//pin para output nos leds
+int LpinLEFT = 2;
+int LpinRIGHT = 4;
+int LpinMID = 7;
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  //RIGHT
+  pinMode(pinRIGHT, INPUT);
+  pinMode(LpinRIGHT, OUTPUT);
+  //LEFT
+  pinMode(pinLEFT, INPUT);
+  pinMode(LpinLEFT, OUTPUT);
+  //MID
+  pinMode(pinMID, INPUT);
+  pinMode(LpinMID, OUTPUT);
+}
+
+void loop() {
+  statusRIGHT = digitalRead(pinRIGHT);
+  statusLEFT = digitalRead(pinLEFT);
+  statusMID = digitalRead(pinMID);
+  
+//RIGHT
+  if ( statusRIGHT == 1){
+    
+    digitalWrite(LpinRIGHT, HIGH);
+    }
+  else{
+
+    digitalWrite(LpinRIGHT, LOW);
+    }
+  Serial.print("RIGHT");
+  Serial.println(statusRIGHT);
+    
+//LEFT  
+  if ( statusLEFT == 1){
+    
+    digitalWrite(LpinLEFT, HIGH);
+    }
+  else{
+
+    digitalWrite(LpinLEFT, LOW);
+    }
+  Serial.print("LEFT");
+  Serial.println(statusLEFT);
+  
+//MID
+  if ( statusMID == 1){
+    
+    digitalWrite(LpinMID, HIGH);
+    }
+  else{
+
+    digitalWrite(LpinMID, LOW);
+    }
+  Serial.print("MID");
+  Serial.println(statusMID);
+}
---
+++ teste_sensor_hall_individual.ino
@@ -0,0 +1,31 @@
+int statusS = 0;
+
+//pin para input proveniente sensores
+int pin= 3;
+
+//pin para output nos leds
+int Lpin= 2;
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  //LEFT
+  pinMode(pin, INPUT);
+  pinMode(Lpin, OUTPUT);
+
+}
+
+void loop() {
+  statusS = digitalRead(pin);
+  //LEFT  
+  if ( statusS == 1){
+    
+    digitalWrite(Lpin, HIGH);
+    }
+  else{
+
+    digitalWrite(Lpin, LOW);
+    }
+  Serial.println(statusS);
+}
