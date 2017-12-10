5f107df4a773c72175a5a63e8ebf05eb98520ce2
AngeloBot <33585124+AngeloBot@users.noreply.github.com>
2017-12-10 16:23:11 -0200

    sensores com configuração recomendada
    capacitor de 100nF e de 4,7nF junto com resistor de 100k, teste dos 4 sensores ao mesmo tempo

---
+++ sensor_hall_latch_teste_full.ino
@@ -0,0 +1,65 @@
+int pass = 0;
+
+// status instant.
+int statusFR;
+int statusR;
+int statusL;
+int statusFL;
+
+//pin FAR RIGHT(amarelo)
+int pinFR = 10;
+
+//pin RIGHT(verde)
+int pinR= 3;
+
+//pin LEFT(amarelo)
+int pinL= 5;
+
+//pin FAR LEFT(verde)
+int pinFL = 9;
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  pinMode(pinR, INPUT);
+  pinMode(pinL, INPUT);
+  pinMode(pinFR, INPUT);
+  pinMode(pinFL, INPUT);
+
+
+}
+
+void loop() {
+
+  pass = 0;
+  
+  statusR = digitalRead(pinR);
+  statusL = digitalRead(pinL);
+  statusFR = digitalRead(pinFR);
+  statusFL = digitalRead(pinFL);
+  
+  if( statusR == 0){ 
+      if (statusFR == 0){
+        Serial.println("Far Right");
+        pass = 1;
+      }
+      else{
+        Serial.println("Right");
+        pass = 1;
+      }
+  }
+  if( statusL == 1 ){
+    if (statusFL == 1){
+      Serial.println("Far Left");
+      pass = 1;
+    }
+    else{
+      Serial.println("Left");
+      pass = 1;
+    }
+  }
+  if (pass == 0){
+    Serial.println("Middle");
+  }
+}
